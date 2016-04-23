8e056f7753db66500dcc9e962b1396e761d99d26
Chen <chen@ChendeMacBook-Pro.local>
2016-04-23 21:50:35 +0800

    mongodb init and try some query test

--- src/api_routes.js
+++ src/api_routes.js
@@ -1,9 +1,37 @@
 var express = require('express');
 var router = express.Router();
 
+var MongoClient = require('mongodb').MongoClient;
+var assert = require('assert');
+var ObjectId = require('mongodb').ObjectID;
+var url = 'mongodb://localhost:27017/test';
+
+var findRestaurants = function(db, callback) {
+   var cursor =db.collection('restaurants').find( );
+   var returnData = '';
+   cursor.each(function(err, doc) {
+      assert.equal(err, null);
+      if (doc != null) {
+         console.dir( doc );
+         returnData += JSON.stringify(doc);
+      } else {
+         callback(returnData);
+      }
+   });
+};
+
+
+
 // define the home page route
 router.get('/', function(req, res) {
-  res.send('Api home page');
+  
+  MongoClient.connect(url, function(err, db) {
+	  assert.equal(null, err);
+	  findRestaurants(db, function(dataset) {
+	      res.send(dataset);
+	  });
+	});
+  
 });
 // define the about route
 router.get('/about', function(req, res) {
